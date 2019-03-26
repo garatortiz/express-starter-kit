18f87ec01b58ac4da5c33cf8a54ede9f6432258b
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2019-03-26 16:20:07 -0300

    Add Babel (ES6) (#6)

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
-var sassMiddleware = require('node-sass-middleware')
+import createError from 'http-errors'
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import sassMiddleware from 'node-sass-middleware'
 
-var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
+import indexRouter from './routes/index'
+import usersRouter from './routes/users'
 
-var app = express()
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, '../views'))
 app.set('view engine', 'pug')
 
 app.use(logger('dev'))
@@ -20,24 +20,22 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(
   sassMiddleware({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
+    src: path.join(__dirname, '../public'),
+    dest: path.join(__dirname, '../public'),
     indentedSyntax: true, // true = .sass and false = .scss
     sourceMap: true,
   })
 )
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404))
-})
+app.use((req, res, next) => next(createError(404)))
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
@@ -47,4 +45,4 @@ app.use(function(err, req, res, next) {
   res.render('error')
 })
 
-module.exports = app
+export default app
