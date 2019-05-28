ae46baa9ea0084242543eb9c276afcc9153a5760
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2019-05-28 15:36:28 -0300

    chore: remove node-sass-middleware

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import express from 'express'
 import path from 'path'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
-import sassMiddleware from 'node-sass-middleware'
 
 import indexRouter from './routes/index'
 import usersRouter from './routes/users'
@@ -12,20 +11,12 @@ const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, '../views'))
-app.set('view engine', 'pug')
+app.set('view engine', 'ejs')
 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(
-  sassMiddleware({
-    src: path.join(__dirname, '../public'),
-    dest: path.join(__dirname, '../public'),
-    indentedSyntax: true, // true = .sass and false = .scss
-    sourceMap: true,
-  })
-)
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.use('/', indexRouter)
